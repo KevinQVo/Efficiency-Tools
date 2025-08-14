ac4e0367a5172d77f4776e20126d9a2f9c7e5795
Kevin <161170945+KevinQVo@users.noreply.github.com>
2025-08-14 14:20:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
 from options import options
 import zTicker
+
+st.set_page_config(page_title="SMA OPS TOOLS")
+st.title("SMA OPS TOOLS")
+
+selection = st.selectbox("Choose Tool Type", options)
+
